660fae1f846cea4cf0ce275af550aa79fa77048c
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-24 13:22:43 +0100

    fc2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <h1>🚧my-wetter-app⛔️🚧</h1>
+    <h1>🚧 my-wetter-app ⛔️ 🚧</h1>
     <h1>☀️❄️🌈⚡️🌤️🌨️    </h1>
     <h3>🚧 ⛔️in kürze entsteht hier ein wetter app! ⛔️🚧</h3>
-    <div>🚧❄️ Home-Page ⛔️🚧</div>
+    <div>🚧 ❄️ Home-Page ⛔️ 🚧</div>
     </>
     
   )
